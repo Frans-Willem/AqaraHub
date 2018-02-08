fa735be39fbaf667f9a53d3bb7ceb8e1b2270398
Frans-Willem Hardijzer <fw@hardijzer.nl>
2018-02-08 19:49:36 +0100

    Reworked MqttWrapper, and added preliminary subscribe support.

--- src/mqtt_wrapper.h
+++ src/mqtt_wrapper.h
@@ -3,6 +3,9 @@
 #include <boost/asio.hpp>
 #include <mqtt_client_cpp.hpp>
 #include <queue>
+#include <set>
+#include <stlab/concurrency/serial_queue.hpp>
+#include "asio_executor.h"
 #include "logging.h"
 
 class MqttWrapper {
@@ -11,44 +14,131 @@ class MqttWrapper {
   virtual stlab::future<void> Publish(
       std::string topic_name, std::string message,
       std::uint8_t qos = mqtt::qos::at_most_once, bool retain = false) = 0;
+  virtual stlab::future<void> Subscribe(
+      std::set<std::tuple<std::string, std::uint8_t>> topics) = 0;
   template <typename F, typename... Args>
   static std::shared_ptr<MqttWrapper> Create(
       F f, boost::asio::io_service& io_service, Args... args);
 };
 
 template <typename C>
-class MqttWrapperImpl : public MqttWrapper {
+class MqttWrapperImpl
+    : public MqttWrapper,
+      public std::enable_shared_from_this<MqttWrapperImpl<C>> {
  public:
   MqttWrapperImpl(boost::asio::io_service& io_service,
                   std::shared_ptr<C> client)
-      : io_service_(io_service), client_(client) {
+      : mutex_queue_(AsioExecutor(io_service)),
+        io_service_(io_service),
+        client_(client) {
     client_->set_client_id("AqaraHub");
     client_->set_clean_session(true);
-    client_->set_connack_handler(std::bind(&MqttWrapperImpl::ConnAckHandler,
-                                           this, std::placeholders::_1,
-                                           std::placeholders::_2));
-	client_->set_pub_res_sent_handler([](std::uint16_t packet_id) {
-			LOG("MqttWrapper", debug) << "pub res sent: " << packet_id;
-			});
-	client_->set_puback_handler(std::bind(&MqttWrapperImpl::PublishAcknowledgedHandler, this, std::placeholders::_1));
-	client_->set_pubcomp_handler(std::bind(&MqttWrapperImpl::PublishCompletedHandler, this, std::placeholders::_1));
-    client_->connect(std::bind(&MqttWrapperImpl::FinishHandler, this,
-                               std::placeholders::_1));
+  }
+  void PostConstructor() {
+    std::weak_ptr<MqttWrapperImpl<C>> weak_this(this->shared_from_this());
+    client_->set_connack_handler([weak_this](auto a, auto b) {
+      if (auto _this = weak_this.lock()) {
+        _this
+            ->mutex_queue_(
+                [weak_this](auto a, auto b) {
+                  if (auto _this = weak_this.lock()) {
+                    _this->ConnAckHandler(a, b);
+                  }
+                },
+                a, b)
+            .detach();
+        return true;
+      }
+      return false;
+    });
+    client_->set_puback_handler([weak_this](auto a) {
+      if (auto _this = weak_this.lock()) {
+        _this
+            ->mutex_queue_(
+                [weak_this](auto a) {
+                  if (auto _this = weak_this.lock()) {
+                    _this->PublishAcknowledgedHandler(a);
+                  }
+                },
+                a)
+            .detach();
+        return true;
+      }
+      return false;
+    });
+    client_->set_pubcomp_handler([weak_this](auto a) {
+      if (auto _this = weak_this.lock()) {
+        _this
+            ->mutex_queue_(
+                [weak_this](auto a) {
+                  if (auto _this = weak_this.lock()) {
+                    _this->PublishCompletedHandler(a);
+                  }
+                },
+                a)
+            .detach();
+        return true;
+      }
+      return false;
+    });
+    client_->connect([weak_this](auto a) {
+      if (auto _this = weak_this.lock()) {
+        _this
+            ->mutex_queue_(
+                [weak_this](auto a) {
+                  if (auto _this = weak_this.lock()) {
+                    _this->FinishHandler(a);
+                  }
+                },
+                a)
+            .detach();
+        return true;
+      }
+      return false;
+    });
+    client_->set_publish_handler([weak_this](auto a, auto b, auto c, auto d) {
+      if (auto _this = weak_this.lock()) {
+        _this
+            ->mutex_queue_(
+                [weak_this](auto a, auto b, auto c, auto d) {
+                  if (auto _this = weak_this.lock()) {
+                    _this->PublishHandler(a, b, c, d);
+                  }
+                },
+                a, b, c, d)
+            .detach();
+        return true;
+      }
+      return false;
+    });
   }
   stlab::future<void> Publish(std::string topic_name, std::string message,
                               std::uint8_t qos, bool retain) override {
+    auto _this = this->shared_from_this();
     auto package = stlab::package<void(std::exception_ptr)>(
-        stlab::immediate_executor, [](std::exception_ptr ex) {
+        AsioExecutor(io_service_), [](std::exception_ptr ex) {
           if (ex) {
             std::rethrow_exception(ex);
           }
         });
-    publish_queue_.push({topic_name, message, qos, retain, package.first});
-    io_service_.post([this]() { this->TrySend(); });
+    PublishQueueItem item{topic_name, message, qos, retain, package.first};
+    return mutex_queue_(
+        [_this](PublishQueueItem item) { _this->SafePublish(item); },
+        std::move(item));
     return package.second;
+  }
+  stlab::future<void> Subscribe(
+      std::set<std::tuple<std::string, std::uint8_t>> topics) override {
+    if (topics.empty()) {
+      return stlab::make_ready_future(AsioExecutor(io_service_));
+    }
+    auto _this = this->shared_from_this();
+    return mutex_queue_([_this](auto x) { _this->SafeSubscribe(x); },
+                        std::move(topics));
   }
 
  private:
+  stlab::serial_queue_t mutex_queue_;
   boost::asio::io_service& io_service_;
   std::shared_ptr<C> client_;
   bool connected_;
@@ -61,77 +151,144 @@ class MqttWrapperImpl : public MqttWrapper {
   };
   std::queue<PublishQueueItem> publish_queue_;
   std::map<std::uint16_t, PublishQueueItem> publish_inprogress_;
+  std::set<std::tuple<std::string, std::uint8_t>> subscriptions_;
 
-  void TrySend() {
+  void SafePublish(PublishQueueItem item) {
     if (!connected_) {
+      publish_queue_.push(std::move(item));
       return;
     }
-	while (!publish_queue_.empty()) {
-		auto item = publish_queue_.front();
-		publish_queue_.pop();
-		auto packet_id = client_->publish(item.topic_name, item.message, item.qos, item.retain);
-		if (item.qos == mqtt::qos::at_most_once) {
-			// No more acknowledgements follow, so resolve the promise.
-			item.callback(nullptr);
-		} else {
-			// Acknowledgements should follow, keep a copy of the item.
-			// TODO: What should we do in case of duplicate packet_id ?
-			publish_inprogress_[packet_id] = item;
-		}
-	}
+    if (item.qos == mqtt::qos::at_most_once) {
+      auto callback = item.callback;
+      client_->acquired_async_publish(
+          0, item.topic_name, item.message, item.qos, item.retain,
+          [callback](const boost::system::error_code& error) {
+            if (error) {
+              callback(std::make_exception_ptr(error));
+            } else {
+              callback(nullptr);
+            }
+          });
+      return;
+    }
+    auto packet_id = client_->acquire_unique_packet_id();
+    auto _this = this->shared_from_this();
+    publish_inprogress_[packet_id] = item;
+    client_->acquired_async_publish(
+        packet_id, item.topic_name, item.message, item.qos, item.retain,
+        [packet_id, _this](const boost::system::error_code& error) {
+          _this
+              ->mutex_queue_(
+                  [_this, packet_id](const boost::system::error_code& error) {
+                    _this->AsyncPublishCallback(packet_id, error);
+                  },
+                  error)
+              .detach();
+        });
   }
 
-  bool ConnAckHandler(bool sp, std::uint8_t connack_return_code) {
+  void SafeSubscribe(
+      std::set<std::tuple<std::string, std::uint8_t>> subscriptions) {
+    subscriptions_.insert(subscriptions.begin(), subscriptions.end());
+    if (connected_) {
+      LOG("MqttWrapper", debug) << "Sending async subscribe directly";
+      client_->async_subscribe(
+          std::vector<std::tuple<std::string, std::uint8_t>>(
+              subscriptions.begin(), subscriptions.end()));
+    }
+  }
+
+  void ConnAckHandler(bool sp, std::uint8_t connack_return_code) {
     if (connack_return_code != mqtt::connect_return_code::accepted) {
       LOG("MqttWrapper", warning)
           << "Connection not accepted: "
           << mqtt::connect_return_code_to_str(connack_return_code);
-      return false;
+      return;
     }
-    LOG("MqttWrapper", debug)
-        << "ConnAckHandler: clean=" << sp;
+    LOG("MqttWrapper", debug) << "ConnAckHandler: clean=" << sp;
     connected_ = true;
-    io_service_.post([this]() { this->TrySend(); });
-    return true;
-  };
+    if (!subscriptions_.empty()) {
+      LOG("MqttWrapper", debug) << "Sending async subscribe after connect";
+      client_->async_subscribe(
+          std::vector<std::tuple<std::string, std::uint8_t>>(
+              subscriptions_.begin(), subscriptions_.end()));
+    }
+    std::queue<PublishQueueItem> queue_copy;
+    std::swap(queue_copy, publish_queue_);
+    while (!queue_copy.empty()) {
+      auto item = queue_copy.front();
+      queue_copy.pop();
+      SafePublish(item);
+    }
+  }
 
   void FinishHandler(const boost::system::error_code& error) {
     LOG("MqttWrapper", debug) << "FinishHandler: " << error;
     connected_ = false;
+    // TODO: Probably trigger reconnect here? Maybe with a small timer ?
   }
 
-  bool PublishAcknowledgedHandler(std::uint16_t packet_id) {
-	  // Indicates a packet with QoS at least once was published succesfully.
-	  auto found = publish_inprogress_.find(packet_id);
-	  if (found == publish_inprogress_.end()) {
-		  LOG("MqttWrapper", debug) << "PublishAcknowledge: Packet ID " << packet_id << " not found";
-		  return true;
-	  }
-	  auto item = found->second;
-	  if (item.qos != mqtt::qos::at_least_once) {
-		  LOG("MqttWrapper", debug) << "PublishAcknowledge: Packet ID " << packet_id << " was supposed to receive PublishComplete...";
-		  return true;
-	  }
-	  publish_inprogress_.erase(found);
-	  item.callback(nullptr);
-	  return true;
+  void AsyncPublishCallback(std::uint16_t packet_id,
+                            const boost::system::error_code& error) {
+    if (error) {
+      auto found = publish_inprogress_.find(packet_id);
+      if (found == publish_inprogress_.end()) {
+        LOG("MqttWrapper", debug)
+            << "AsyncPublishCallback: Packet ID " << packet_id << " not found";
+        return;
+      }
+      auto item = found->second;
+      publish_inprogress_.erase(found);
+      item.callback(std::make_exception_ptr(error));
+      return;
+    }
   }
 
-  bool PublishCompletedHandler(std::uint16_t packet_id) {
-	  // Indicates a packet with QoS exactly once was published succesfully.
-	  auto found = publish_inprogress_.find(packet_id);
-	  if (found == publish_inprogress_.end()) {
-		  LOG("MqttWrapper", debug) << "PublishComplete: Packet ID " << packet_id << " not found";
-		  return true;
-	  }
-	  auto item = found->second;
-	  if (item.qos != mqtt::qos::exactly_once) {
-		  LOG("MqttWrapper", debug) << "PublishComplete: Packet ID " << packet_id << " was supposed to receive PublishAcknowledge...";
-		  return true;
-	  }
-	  publish_inprogress_.erase(found);
-	  item.callback(nullptr);
-	  return true;
+  void PublishAcknowledgedHandler(std::uint16_t packet_id) {
+    // Indicates a packet with QoS at least once was published succesfully.
+    auto found = publish_inprogress_.find(packet_id);
+    if (found == publish_inprogress_.end()) {
+      LOG("MqttWrapper", debug)
+          << "PublishAcknowledge: Packet ID " << packet_id << " not found";
+      return;
+    }
+    auto item = found->second;
+    if (item.qos != mqtt::qos::at_least_once) {
+      LOG("MqttWrapper", debug)
+          << "PublishAcknowledge: Packet ID " << packet_id
+          << " was supposed to receive PublishComplete...";
+      return;
+    }
+    publish_inprogress_.erase(found);
+    item.callback(nullptr);
+    return;
+  }
+
+  void PublishCompletedHandler(std::uint16_t packet_id) {
+    // Indicates a packet with QoS exactly once was published succesfully.
+    auto found = publish_inprogress_.find(packet_id);
+    if (found == publish_inprogress_.end()) {
+      LOG("MqttWrapper", debug)
+          << "PublishComplete: Packet ID " << packet_id << " not found";
+      return;
+    }
+    auto item = found->second;
+    if (item.qos != mqtt::qos::exactly_once) {
+      LOG("MqttWrapper", debug)
+          << "PublishComplete: Packet ID " << packet_id
+          << " was supposed to receive PublishAcknowledge...";
+      return;
+    }
+    publish_inprogress_.erase(found);
+    item.callback(nullptr);
+    return;
+  }
+
+  void PublishHandler(std::uint8_t fixed_header,
+                      boost::optional<std::uint16_t> packet_id,
+                      std::string topic_name, std::string message) {
+    LOG("MqttWrapper", debug)
+        << "PublishHandler: " << topic_name << ": " << message;
   }
 };
 
@@ -141,7 +298,9 @@ std::shared_ptr<MqttWrapper> MqttWrapper::Create(
   typedef typename std::result_of<F(boost::asio::io_service&, Args...)>::type
       ReturnType;
   typedef typename ReturnType::element_type ClientType;
-  return std::make_shared<MqttWrapperImpl<ClientType>>(io_service,
-                                                       f(io_service, args...));
+  auto wrapper = std::make_shared<MqttWrapperImpl<ClientType>>(
+      io_service, f(io_service, args...));
+  wrapper->PostConstructor();
+  return wrapper;
 }
 #endif  // _MQTT_WRAPPER_H_
